e8fa5aebe140921fab6224d6c6836c16e9ece5e2
Will Parks <wparks08@gmail.com>
2020-01-31 12:25:20 -0800

    add authentication and login form

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 var express = require("express");
 var exphbrs = require("express-handlebars");
+var session = require("express-session");
+var passport = require("passport");
+var authentication = require("./controllers/authentication");
+
 var db = require("./models");
 
 // Setup app
@@ -11,6 +15,12 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport config
+authentication.config(passport);
 
 // Initialize handlebars
 var hbrsOptions = {
@@ -22,6 +32,7 @@ app.engine("handlebars", exphbrs(hbrsOptions));
 app.set("view engine", "handlebars");
 
 // Add routes
+require("./routes/adminRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Set sequelize options
