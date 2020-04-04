f016ac6d248209851daf6ec7dad4ef3bfd14a3e9
Will Parks <wparks08@gmail.com>
2020-04-04 15:14:39 -0700

    move server config values to config.json

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const session = require("express-session");
 const passport = require("passport");
 const fileUpload = require("express-fileupload");
 const authentication = require("./controllers/authentication");
-
+const config = require("./config/config");
 const db = require("./models");
 
 // Setup app
@@ -13,41 +13,27 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded(config.urlencoded.options));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(config.static.folder));
 app.use(session({ secret: process.env.SESSION_SECRET }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(
-    fileUpload({
-        limits: { fileSize: 50 * 1024 * 1024 }
-    })
-);
+app.use(fileUpload(config.fileUpload.options));
 
 // Passport config
 authentication.config(passport);
 
-// Initialize handlebars
-const hbrsOptions = {
-    defaultLayout: "main",
-    allowProtoPropertiesByDefault: true
-};
-
-app.engine("handlebars", exphbrs(hbrsOptions));
-app.set("view engine", "handlebars");
+// Initialize view engine
+app.engine(config.viewEngine.name, exphbrs(config.viewEngine.options));
+app.set("view engine", config.viewEngine.name);
 
 // Add routes
 require("./routes/adminRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// Set sequelize options
-const syncOptions = {
-    force: false
-};
-
 // Sync models and start server
-db.sequelize.sync(syncOptions).then(() => {
+db.sequelize.sync(config.sequelize.sync).then(() => {
     app.listen(PORT, () => {
         console.log("Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
     });
